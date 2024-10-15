5e966763700a7184c3562308d6ca1ffb9a3de9e3
Davo0416 <60065631+Davo0416@users.noreply.github.com>
2024-10-15 17:09:13 +0100

    Add files via upload

---
+++ calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author davog
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        CalcFrame calculator = new CalcFrame();
+        calculator.setVisible(true);
+    }
+}
